61f666cf95a1027d8810b8c0588fbd584c465b5d
x0f5c3 <x0f5c3@tutanota.com>
2021-11-09 19:52:06 +0100

    More persistence work, need to choose between TOML and JSON

--- src/decompressor.rs
+++ src/decompressor.rs
@@ -1,15 +1,40 @@
-use std::io::{Read, Seek};
+use crate::{Error, Result};
+use std::io::{BufRead, Read, Seek};
+use std::path::Path;
 
 #[cfg(not(target_os = "windows"))]
-use async_compression::tokio::bufread::GzipDecoder;
-use tokio::io::AsyncBufRead;
+use flate2::bufread::GzDecoder;
+#[cfg(not(target_os = "windows"))]
+use tar::Archive;
 
-struct ToDecompress<R>
+pub struct ToDecompress<R>
 where
-    R: Read + Seek + AsyncBufRead,
+    R: Read + Seek + BufRead,
 {
     #[cfg(target_os = "windows")]
     decompressor: zip::ZipArchive<R>,
     #[cfg(not(target_os = "windows"))]
-    decompressor: GzipDecoder<R>,
+    decompressor: Archive<GzDecoder<R>>,
+}
+
+impl<R: Read + Seek + BufRead> ToDecompress<R> {
+    #[cfg(target_os = "windows")]
+    pub(crate) fn new(inner: R) -> Result<Self> {
+        Ok(Self {
+            decompressor: zip::ZipArchive::new(inner)?,
+        })
+    }
+    #[cfg(not(target_os = "windows"))]
+    pub(crate) fn new(inner: R) -> Result<Self> {
+        let dec = tar::Archive::new(GzDecoder::new(inner));
+        Ok(Self { decompressor: dec })
+    }
+    #[cfg(target_os = "windows")]
+    pub(crate) fn extract(&mut self, path: &Path) -> Result<()> {
+        self.decompressor.extract(path).map_err(Error::ZIPErr)
+    }
+    #[cfg(not(target_os = "windows"))]
+    pub(crate) fn extract(&mut self, path: &Path) -> Result<()> {
+        self.decompressor.unpack(path).map_err(Error::IOError)
+    }
 }
